ee2b13b1fea9f927103f6f1f89fbd982d2db4411
salmaanrizvi <sar228@cornell.edu>
2016-06-16 15:35:27 -0400

    all tests passed

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,6 +3,9 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *topLabel;
+@property (strong, nonatomic) IBOutlet UILabel *middleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *bottomLabel;
 
 @end
 
@@ -21,5 +24,30 @@
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+
+- (IBAction)threeOfSpades:(id)sender {
+    self.topLabel.text = self.threeOfSpades.cardLabel;
+    self.middleLabel.text = self.threeOfSpades.cardLabel;
+    self.bottomLabel.text = self.threeOfSpades.cardLabel;
+}
+
+- (IBAction)fourOfClubs:(id)sender {
+    self.topLabel.text = self.fourOfClubs.cardLabel;
+    self.middleLabel.text = self.fourOfClubs.cardLabel;
+    self.bottomLabel.text = self.fourOfClubs.cardLabel;
+}
+
+- (IBAction)eightOfDiamonds:(id)sender {
+    self.topLabel.text = self.eightOfDiamonds.cardLabel;
+    self.middleLabel.text = self.eightOfDiamonds.cardLabel;
+    self.bottomLabel.text = self.eightOfDiamonds.cardLabel;
+}
+
+- (IBAction)tenOfHearts:(id)sender {
+    self.topLabel.text = self.tenOfHearts.cardLabel;
+    self.middleLabel.text = self.tenOfHearts.cardLabel;
+    self.bottomLabel.text = self.tenOfHearts.cardLabel;
+}
+
 
 @end
